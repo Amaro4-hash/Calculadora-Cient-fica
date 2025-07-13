ee03fbc6cdafc2072249c9f08c5740281cd65e7d
Amaro Dias <amrodias924@gmail.com>
2025-07-13 09:19:43 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "calculadora.h"
+
+static void menu(){
+    
+int opcao;
+bool value = true;
+do{
+switch (opcao) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    default:
+    
+}
+
+}while(value);
+}
+
+int main(){
+    
+menu();    
+    return 0;
+}
