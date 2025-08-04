8664d12af24f169464f17204767965b0ad5ae708
Amaro Dias <amrodias924@gmail.com>
2025-08-04 08:52:44 +0100

    Create prog.c

---
+++ prog.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+string INPUT(string texto[], int tam)
+{
+    fgets(texto);
+    fflush(stdin);
+    
+    
+}
+
+string OUTPUT (string texto[], int tam)
+{
+    return texto;
+}
+
+void IF ()
+{
+    
+}
+
+WHILE ()
+{
+    
+    
+}
+
+FOR ()
+{
+    
+}
